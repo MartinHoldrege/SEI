3beac2ac8ff7c5885f9f416c3440518beae8ae91
martinholdrege <martinholdrege@gmail.com>
2022-02-15 22:09:57 +0000

    Updated scripts/02_SEI_transitions_current-vs-future.js (added 70 lines).

--- scripts/02_SEI_transitions_current-vs-future.js
+++ scripts/02_SEI_transitions_current-vs-future.js
@@ -41,6 +41,17 @@ var c9Palette = ['#000000', // stable core (black)
   '#B1CE94', // impacted becomes grow
   '#D9D9D9' // stable impacted
 ];
+var c9Names = [
+  'Stable core',
+  'Core becomes grow',
+  'Core becomes impacted',
+  'Grow becomes core',
+  'Stable grow',
+  'Grow becomes impacted',
+  'Impacted becomes core',
+  'Impacted becomes grow',
+  'Stable impacted'
+  ];
 var imageVisc9 = {"opacity":1,"min":1,"max":9, "palette":c9Palette};
 
 // Read in data -------------------------------------------------------
@@ -86,3 +97,62 @@ var statesOutline = empty.paint({
 Map.addLayer(ee.Image(1), {'min':1, 'max':1, palette: "white"},'background'); // white background
 Map.addLayer(statesOutline, {}, 'outline'); // outline of states
 Map.addLayer(c9b, imageVisc9, 'c9 transition');
+
+// Legend --------------------------
+
+// set position of panel
+var legend = ui.Panel({
+  style: {
+    position: 'bottom-left',
+    padding: '8px 15px'
+  }
+});
+ 
+// Create legend title
+var legendTitle = ui.Label({
+  value: 'Transition',
+  style: {
+    fontWeight: 'bold',
+    fontSize: '18px',
+    margin: '0 0 4px 0',
+    padding: '0'
+    }
+});
+ 
+// Add the title to the panel
+legend.add(legendTitle);
+ 
+// Creates and styles 1 row of the legend.
+var makeRow = function(color, name) {
+ 
+      // Create the label that is actually the colored box.
+      var colorBox = ui.Label({
+        style: {
+          backgroundColor: color,
+          // Use padding to give the box height and width.
+          padding: '8px',
+          margin: '0 0 4px 0'
+        }
+      });
+ 
+      // Create the label filled with the description text.
+      var description = ui.Label({
+        value: name,
+        style: {margin: '0 0 4px 6px'}
+      });
+ 
+      // return the panel
+      return ui.Panel({
+        widgets: [colorBox, description],
+        layout: ui.Panel.Layout.Flow('horizontal')
+      });
+};
+ 
+
+// Add color and and names
+for (var i = 0; i < c9Palette.length; i++) {
+  legend.add(makeRow(c9Palette[i], c9Names[i]));
+  }  
+ 
+// add legend to map (alternatively you can also print the legend to the console)
+Map.add(legend);
