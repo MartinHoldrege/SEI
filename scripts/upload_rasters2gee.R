b3df3d7f41b0f5bf88b2228c50d331074f56ef06
Martin Holdrege <martinholdrege@gmail.com>
2022-02-18 16:39:58 -0700

    script not working because of system() path(?) issues.

--- scripts/upload_rasters2gee.R
+++ scripts/upload_rasters2gee.R
@@ -11,8 +11,10 @@
 # earthengine command line tool 
 # (https://developers.google.com/earth-engine/guides/command_line).
 # I'm just calling that tool from R. It
-# was installed when I installed the rgee package. 
+# was installed when I installed anaconda3 
+# (https://developers.google.com/earth-engine/guides/python_install-conda).
 
+# NOTE--this code is not working at this time. Use upload_rasters2gee.sh instead
 
 # dependencies ------------------------------------------------------------
 
@@ -30,15 +32,36 @@ fp <- list.files("data_raw/stepwat_change_rasters/")
 # path to files in gee
 
 # removing .tif ending of file
-asset_id <- paste0("users/MartinHoldrege/SEI/stepwat_change_rasters/",
+asset_id <- paste0("projects/gee-guest/assets/SEI/stepwat_change_rasters/",
                    str_replace(fp, ".tif$", ""))
 
 # google cloud platform paths ----------------------------------------------
 
 # locations on gcp
-gcp_path <- paste0("gs://stepwat_change_rasters/", fp)
+gcp_path <- paste0("gs://mholdrege/", fp)
 
 
+# make sure earthengine command working -----------------------------------
+
+path <- Sys.getenv("PATH")
+
+# Path to Anaconda3 scripts folder must be present for 
+# earthengine commandline program to run
+# not sure why this change doesn't seem to be permanent
+
+conda_path <- "C:\\Users\\mholdrege\\Anaconda3\\scripts"
+
+if(!str_detect(path, "Anaconda3.*scripts")) {
+  path <- c(conda_path, path) 
+  path <- paste(path,collapse=";")
+  Sys.setenv(Path=path)
+}
+ #system('conda init bash')
+x <- system("conda activate ee") # this code isn't working at the moment. 
+# system('CALL conda.bat activate')
+if(x != 1) {
+  stop("conda ee not working")
+}
 # transfer files ----------------------------------------------------------
 
 # command line earthengine commands
@@ -50,10 +73,8 @@ commands <- paste0("earthengine upload image --asset_id=",
 # and the earth engine API, and connected that to git bash. as a result the
 # earthengine command line tool works. 
 
-system("conda activate") # this code isn't working at the moment. 
-system('earthengine') # test that working
-system("ls")
 for (com in commands) {
   system(com)
 }
-# system(command)
+system(paste("conda activate ee;", commands[2]))
+system(paste(commands[1]))
