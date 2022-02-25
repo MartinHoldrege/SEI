d940052f7be9689f55ec5afa521747c1c54cb1f6
Martin Holdrege <martinholdrege@gmail.com>
2022-02-25 11:48:45 -0700

    Mde first draft of c9 maps from raster that that was outputed to gdrive.

--- scripts/03_SEI_transitions_maps.R
+++ scripts/03_SEI_transitions_maps.R
@@ -1,38 +1,32 @@
+# Martin Holdrege
 
+# Script started 2/25/2022
+
+# Purpose: create maps of transitions in sagebrush ecosystem integrity
+# from current to future conditions. These are 9 classes.
+
+# Next steps
+# --update colors to match the newer map that John has
+# but make impacted becomes core a mustard yellow
+# and make the one blue color a deeper blue
+
+# also add a background (state polygon) to these maps. 
 
 # dependencies ------------------------------------------------------------
 
-library(rgee)
 library(terra)
 library(tmap)
 
-# setup ee' ---------------------------------------------------------------
+# read in data ------------------------------------------------------------
 
-# before changing users I needed to run ee_clean_credentials()
-# I'm not sure why but I'm needing to re-authenticate most times I 
-# re-open R. When it works it pops up a google chrome window to sign
-# into the google account
-# ee_Initialize('mholdrege')
-ee_Initialize()
+# current to future SEI transition, file created in
+# 02_SE_transitions_current-vs-future.js
+c9 <- rast("data_processed/transitions/SEIv11_9ClassTransition_1000_byScenario_median_20220224.tif")
 
-# read in the data --------------------------------------------------------
-ee_path <- 'projects/gee-guest/assets/SEI/'
-
-c9_path <- paste0(
-  ee_path, 'v11/transitions/SEIv11_9ClassTransition_byScenario_median_20220224')
-
-c9 <- ee$Image(c9_path)
-bands <- c9$bandNames()
-
-# region of interests -----------------------------------------------------
-
-#  defines the study region
-biome <-  ee$FeatureCollection(paste0(ee_path, 'US_Sagebrush_Biome_2019'))
-region <- biome$geometry()$bounds() # creating a box around the polygon
 
 # visualization params ----------------------------------------------------
 
-c9Palette = c('#000000', # stable core (black)
+c9Palette = c(   '#000000', # stable core (black)
                  '#64AC46', # core becomes grow
                  '#ABAB4B', # core becomes impacted
                  '#2159B0', # grow becomes core
@@ -54,54 +48,39 @@ c9Names <-  c(
   'Stable other'
 )
 
-imageVisc9 <- list(
-  min = 0,
-  max = 9,
-  palette = c('orange', c9Palette))
 
-image <- c9b$select(ee$String(bands$get(0)))$unmask(ee$Image(0))
-isZero <- image$eq(0)
-Map$centerObject(image)
-Map$addLayer(
-  eeObject = image,
-  visParams = imageVisc9,
-  name = '9 transitions'
-)
 
-reduction <- image$reduceRegion(ee$Reducer$frequencyHistogram(), 
-                                geometry = region,
-                                bestEffort = TRUE);
-reduction$getInfo()
+# maps --------------------------------------------------------------------
 
-rast <- ee_as_thumbnail(
-  image = image,  # Image to export
-  region = region, # Region of interest
-  dimensions = 1024, # output dimension
-  raster = TRUE,
-  vizparams = list(min = 0, max = 9)
-)
-x <- values(rast)
-length(unique(x))
-unique(x)
-crs(rast) <- image$projection()$crs()$getInfo()
 
-plot(rast)
+bands <- names(c9)
+c9[c9==0] <- NA # 0s are the values that were originally masked
 
-table(x)
-rast[] <-scales::rescale(
-  values(rast), c(0, 9)
-)
 
-table(values(rast))
-
-stopifnot(all(values(rast) %in%  0:9))
-
-ggplot()
 tmap_mode('plot')
-tm_shape(shp = rast) +
-  tm_raster(
-    title = "LST (°C)",
-    breaks = -1:9 + 0.5,
-  ) 
 
-plot(rastZero)
+# the 1000 refers to the resolution
+pdf("figures/maps_transition/c9_transition_1000_byScenario_v1.pdf")
+
+for (i in seq_along(bands)) {
+print(
+  tm_shape(shp = c9[bands[i]]) +
+    tm_raster(
+      title = "Transition",
+      breaks = -0:9 + 0.5,
+      palette = c9Palette,
+      labels = c9Names
+    ) +
+    tm_layout(
+      legend.outside = TRUE,
+      legend.text.size = 0.75,
+      main.title = bands[i],
+      main.title.size = 0.75
+    ) +
+    tmap_options( # increase number of pixels plotted
+      max.raster = c(plot = 1e10, view = 1e6)
+    )
+)
+}
+
+dev.off()
