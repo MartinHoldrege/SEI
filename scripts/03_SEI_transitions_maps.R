042565bdb03ef78224d79f1268a732a6e38a0f11
Martin Holdrege <martinholdrege@gmail.com>
2022-03-03 13:00:46 -0700

    c9 palette changed, color matrix and state polygons added to c9 maps

--- scripts/03_SEI_transitions_maps.R
+++ scripts/03_SEI_transitions_maps.R
@@ -14,8 +14,10 @@
 
 # dependencies ------------------------------------------------------------
 
+library(tidyverse)
 library(terra)
 library(tmap)
+library(spData) # for us_states polygon
 source("src/fig_params.R")
 
 # read in data ------------------------------------------------------------
@@ -25,21 +27,56 @@ source("src/fig_params.R")
 c9 <- rast("data_processed/transitions/SEIv11_9ClassTransition_1000_byScenario_median_20220224.tif")
 
 
-# visualization params ----------------------------------------------------
+# create 9 color matrix ---------------------------------------------------
+# Creating a 3x3 colored matrix of current and future SEI classes,
 
-c9Palette = c(   '#000000', # stable core (black)
-                 '#64AC46', # core becomes grow
-                 '#ABAB4B', # core becomes impacted
-                 '#2159B0', # grow becomes core
-                 '#757170', # stable grow
-                 '#F0FA77', # grow becomes impacted
-                 '#7698D8', # impacted becomes core
-                 '#B1CE94', # impacted becomes grow
-                 '#D9D9D9') # stable impacted
+c3_levels <- c('CSA', 'GOA', 'ORA')
 
+# c9 levels, with there associated current and future SEI categories
+df_c9 <- tibble(
+  c9Name  = c9Names, 
+  c9Value = 1:9,
+  current = rep(c3_levels, each = 3), # Current SEI (3 levels)
+  future = rep(c3_levels, 3) # future SEI (3 levels)
+ ) %>% 
+  mutate(
+    current = factor(current, levels = rev(c3_levels)),
+    future = factor(future, levels = c3_levels)
+  )
+
+# Adding label column (category of change)
+df_c9$label <- NA
+df_c9$label[c(1, 5, 9)] <- "Stable"
+df_c9$label[c(2, 3, 6)] <- "Declining"
+df_c9$label[c(4, 7, 8)] <- "Increasing"
+
+df_c9
+
+# color of text in color matrix
+text_color <- rep('black', 9)
+text_color[c(1, 5)] <- 'white' # background is dark
+names(text_color) <- c9Names
+
+color_matrix <- ggplot(df_c9, aes(future, current, fill = c9Name)) +
+  geom_tile() +
+  geom_text(aes(label = label, color = c9Name)) +
+  theme_minimal() +
+  scale_x_discrete(position = 'top') +
+  scale_fill_manual(values = c9Palette) +
+  labs(x = "Future condition",
+       y = "Current condition") +
+  scale_color_manual(values = text_color)+
+  theme(panel.grid = element_blank(),
+        legend.position = 'none')
+color_matrix
+png("figures/c9_color_matrix.png",
+     width = 3.5, height = 2.5, units = 'in',
+     res = 600)
+color_matrix
+dev.off()
 
 # maps --------------------------------------------------------------------
-
+# Maps color coded by transition category, for each climate scenario 
 
 bands <- names(c9)
 c9[c9==0] <- NA # 0s are the values that were originally masked
@@ -47,28 +84,37 @@ c9[c9==0] <- NA # 0s are the values that were originally masked
 
 tmap_mode('plot')
 
-# the 1000 refers to the resolution
-pdf("figures/transition_maps/c9_transition_1000_byScenario_v1.pdf")
+# creating a larger bounding box
+bbox <- tmaptools::bb(x = raster::raster(c9), ext = 1.05) 
+bbox['xmin'] <- bbox['xmin'] - 5 # extend left margin of bbox
 
+
+# the 1000 refers to the resolution
+pdf("figures/transition_maps/c9_transition_1000_byScenario_v2.pdf")
+# creating one map for each climate scenario (band in the raster)
 for (i in seq_along(bands)) {
-print(
-  tm_shape(shp = c9[bands[i]]) +
+  map <- tm_shape(shp = c9[bands[i]], bbox = bbox) +
     tm_raster(
       title = "Transition",
       breaks = -0:9 + 0.5,
       palette = c9Palette,
-      labels = c9Names
-    ) +
-    tm_layout(
-      legend.outside = TRUE,
-      legend.text.size = 0.75,
-      main.title = bands[i],
-      main.title.size = 0.75
+      legend.show = FALSE
     ) +
     tmap_options( # increase number of pixels plotted
-      max.raster = c(plot = 1e10, view = 1e6)
-    )
-)
-}
-
+      max.raster = c(plot = 1e10, view = 1e6) 
+    ) +
+    tm_logo("figures/c9_color_matrix.png",
+            position = c("LEFT", "BOTTOM"), height = 7) +
+  tm_shape(us_states) +
+    tm_borders() +
+  tm_layout(
+    legend.outside = TRUE,
+    legend.text.size = 0.75,
+    main.title = bands[i],
+    main.title.size = 0.75,
+    frame = FALSE) 
+  
+  print(map)
+}      
 dev.off()
+
--- src/fig_params.R
+++ src/fig_params.R
@@ -3,16 +3,6 @@
 # Various parameters used for figure making
 
 
-# colors ------------------------------------------------------------------
-
-cols_rcp <- c(#"Current" = "cornflower blue",
-              "RCP45" = "darkgreen",
-              "RCP85" = "green3")
-
-# misc --------------------------------------------------------------------
-
-
-
 # Names of the 9 transition categories
 c9Names <-  c(
   'Stable core',
@@ -25,3 +15,27 @@ c9Names <-  c(
   'Other becomes grow',
   'Stable other'
 )
+
+# colors ------------------------------------------------------------------
+
+cols_rcp <- c(#"Current" = "cornflower blue",
+              "RCP45" = "darkgreen",
+              "RCP85" = "green3")
+
+# color palette based on colorbrewer2 8-class RdBu palette
+c9Palette = c('#000000', # stable core (black)
+             '#f4a582', # core becomes grow
+             '#b2182b', # core becomes impacted
+             '#92c5de', # grow becomes core
+             '#757170', # stable grow
+             '#d6604d', # grow becomes impacted
+             '#2166ac', # impacted becomes core
+             '#4393c3', # impacted becomes grow
+             '#D9D9D9') # stable impacted
+
+names(c9Palette) <- c9Names
+
+
+
+
+
