d2fdb2c269121c874eb58cd71bfc6a48c9151dfb
Martin Holdrege <martinholdrege@gmail.com>
2023-03-02 14:53:02 -0700

    initial code created for moving assets from gee-guest project to the drylandecohydrology project

---
+++ scripts/mv_assets/02_prepare-paths.R
@@ -0,0 +1,66 @@
+# Martin Holdrege
+
+# started March 2, 2023
+
+# purpose: creating the necessary asset paths and folder paths
+# for migrating assests (and creating new folders) in the new
+# google earth engine cloud project
+
+# steps:
+# 1) create list of all folders and sub-folders (which will need to be)
+# created in the new project
+
+# 2) creat list of all file paths (with base of file path that is specific
+# to the project removed)
+
+# 3) save thes as text files
+
+
+# dependencies ------------------------------------------------------------
+
+library(tidyverse)
+
+# read in data ------------------------------------------------------------
+
+# this file contains the paths of all assets in the old gee-guest
+# project, but also the folder names
+l1 <- read_lines("data_processed/ee_asset_paths/old_asset_paths.txt")
+
+# functions ---------------------------------------------------------------
+
+# recursive function that returns all the directories
+# and their directories--doing this b/ earthengine command line 
+# tool cannot create folders inside of folders that alread exist
+all_folder_names <- function(x) {
+  out <- unique(dirname(x))
+  out <- out[out!= "."]
+  if(length(out) == 0)
+    return(x)
+  else {
+    return(sort(unique(c(x, out, all_folder_names(out)))))
+  }
+}
+
+
+# 1: determine folder names --------------------------------------------------
+# need to try and seperate file from 
+
+folders1 <- str_subset(l1, "\\[Folder\\]") %>% 
+  str_extract("(?<=gee-guest/assets/).*") %>% 
+  sort()
+
+folders2 <- all_folder_names(folders1)
+
+# 2: file names -------------------------------------------------------------
+
+files1 <- str_extract(l1, "(?<=gee-guest/assets/).*") 
+
+# removing files that are actually folders
+files2 <- files1[!files1%in% folders2]
+
+files2 <- str_subset(files2, "SEI") # for now just want to move the SEI assets
+
+# 3: write files -------------------------------------------------------------
+
+write_lines(folders2, "data_processed/ee_asset_paths/folders2create.txt")
+write_lines(files2, "data_processed/ee_asset_paths/files2mv.txt")
---
+++ scripts/mv_assets/03_move-gee-assets.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# purpose
+# script to move earth engine assets from gee-guest to usgs-gee-drylandecohydrology cloud project
+# not b/ of some unknown issues the bash shell within rstudio isn't behaving correctly
+# so this script needs be run directly from git bash
+
+# Author: Martin Holdrege
+
+# Script started: March 2, 2023
+
+# set up environment -----------------------
+
+cd
+source $".bashrc" # b/ of aliases etc. for conda environment
+conda activate ee # earthengine commandline environment
+
+# earthengine set_project usgs-gee-drylandecohydrology # I don't think this line needs to run
+
+new="projects/usgs-gee-drylandecohydrology/assets/"
+old="projects/gee-guest/assets/"
+
+# create folders
+
+paths_fold="/c/Users/mholdrege/OneDrive - DOI/Documents/projects/SEI/data_processed/ee_asset_paths/folders2create.txt"
+
+# uncomment these lines to run
+# while read p; do
+#  earthengine create folder ${new}${p}
+#done <"$paths_fold"
+
+# initially only running these for the SEI assets
+paths_file="/c/Users/mholdrege/OneDrive - DOI/Documents/projects/SEI/data_processed/ee_asset_paths/files2mv.txt"
+while read p; do
+# using cp instead of mv for safety (can delete assets from gee-guest later)
+ earthengine cp ${old}${p} ${new}${p}
+done <"$paths_file"
