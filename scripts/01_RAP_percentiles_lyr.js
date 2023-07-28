5563aac01b03d744c52a383635ff712954dfba38
martinholdrege <martinholdrege@gmail.com>
2023-07-28 23:22:57 +0000

    Updated scripts/01_RAP_percentiles_lyr.js (added 36 lines, deleted 10 lines).

--- scripts/01_RAP_percentiles_lyr.js
+++ scripts/01_RAP_percentiles_lyr.js
@@ -9,9 +9,11 @@ Date started: July 28, 2023
 
 // Params ---------------------------------------
 
-var resolution = 90
-var yearEnd = 2021  
-var yearStart = 1986
+var resolution = 90;
+var yearEnd = 2021;
+var yearStart = 1986;
+var radius = 2000; // how much to smooth (meters)
+var dateString = '20230728';
 
 // Dependencies ---------------------------------
 
@@ -22,7 +24,7 @@ var SEI = require("users/mholdrege/SEI:src/SEIModule.js");
 var rapIc = ee.ImageCollection('projects/rangeland-analysis-platform/vegetation-cover-v3')
   .filterDate(yearStart + '-01-01',  yearEnd + '-12-31')
   .map(function(x) {
-    return ee.Image(x).mask(SEI.mask).select(["PFG"]);
+    return ee.Image(x).mask(SEI.mask).select(["PFG", "AFG"]);
   });
   
 var rcmapIc = ee.ImageCollection("USGS/NLCD_RELEASES/2019_REL/RCMAP/V5/COVER")
@@ -30,13 +32,37 @@ var rcmapIc = ee.ImageCollection("USGS/NLCD_RELEASES/2019_REL/RCMAP/V5/COVER")
   .filter(ee.Filter.lte('system:index', ee.Number(yearEnd).format('%d')))
 // .filterDate(yearStart + '-01-01',  yearEnd + '-12-31')
   .map(function(x) {
-    return ee.Image(x).mask(SEI.mask).select('rangeland_sagebrush');
+    return ee.Image(x).mask(SEI.mask).select(['rangeland_sagebrush'], ['sagebrush']);
   });
   
 // summarize across years -------------------------------------
 
-var reducer = ee.Reducer.percentile([95]);
+var reducer = ee.Reducer.percentile([50, 95]); //calculating median and 95 percentile
 var rap1 = rapIc.reduce(reducer);
-print(rap1)
-Map.addLayer(rapIc.mean(), {min:0, max: 50, palette: ['white', 'green']}, 'rap mean')
-Map.addLayer(rap1, {min:0, max: 50, palette: ['white', 'green']}, 'rap 9th')
+var rcmap1 = rcmapIc.reduce(reducer);
+
+Map.addLayer(rapIc.select('PFG').mean(), {min:0, max: 75, palette: ['white', 'green']}, 'rap mean', false);
+Map.addLayer(rap1.select("PFG_p95"), {min:0, max: 75, palette: ['white', 'green']}, 'rap 95th', false);
+
+// smooth spatially ---------------------------------------------
+
+var rapSmooth1 = rap1
+  .reduceNeighborhood(reducer,ee.Kernel.gaussian(radius, radius,'meters'),null, false);
+  
+var rapSmooth2 = rapSmooth1.select(['PFG_p50_p50', 'PFG_p95_p95', 'AFG_p50_p50'], 
+                                    ['PFG_p50', 'PFG_p95', 'AFG_p50']);
+                                    
+var rcmapSmooth1 = rcmap1
+  .reduceNeighborhood(reducer,ee.Kernel.gaussian(radius, radius,'meters'),null, false)
+  .select(['sagebrush_p50_p50', 'sagebrush_p95_p95'], ['sagebrush_p50', 'sagebrush_p95']);
+                               
+
+print(rapSmooth2);
+print(rcmapSmooth1);
+
+// combine layers -----------------------------------------------
+
+var comb1 = rapSmooth2.addBands(rcmapSmooth1);
+
+// write output -----------------------------------------------
+
