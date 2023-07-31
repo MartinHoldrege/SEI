84b6518fa2b337b3a204b3e666089f7d0e5b4214
martinholdrege <martinholdrege@gmail.com>
2023-07-31 21:18:29 +0000

    Updated scripts/02_RAP_percentiles_2drive.js (added 39 lines, deleted 6 lines).

--- scripts/02_RAP_percentiles_2drive.js
+++ scripts/02_RAP_percentiles_2drive.js
@@ -1,14 +1,47 @@
+/*
+  Purpose:
+  write asset of(smoothed mediant median and 95 percentile) cover created in 01_RAP_percentiles_lyr.js.
+  That asset was created at a fairly high resolution (90 m), so the 95th percentiles would be accurate
+  Here outputting it at a coarser resolution for later use, but because pyramiding is based
+  on average (here averages of the correct percentiles calculated at a finer scale) the 
+  coarse res values should be accurate.
+  
+  Author: Martin Holdrege
+  
+  Started: July 31, 2023
+*/
+
+// Dependencies ---------------------------------
+
+var SEI = require("users/mholdrege/SEI:src/SEIModule.js");
+
+// params
+
+var scale = 1000;
+var yearEnd = 2021;
+var yearStart = 1986;
+var radius = 2000; // how much to smooth (meters)
+var dateString = '20230728';
+// var path = SEI.path;
+var path = 'users/MartinHoldrege/SEI/';
+
+// read in asset -----------------------------------------------
+var fileName = 'cover_rap-rcmap_' + yearStart + '_' + yearEnd + '_' + 90 + 'm_' + radius + 'msmooth_' + dateString;
+
+var comb1 = ee.Image(path + 'cover/' + fileName);
 
 
 
+// write to drive ----------------------------
+var fileNameOut = 'cover_rap-rcmap_' + yearStart + '_' + yearEnd + '_' + scale + 'm_' + radius + 'msmooth_' + dateString;
 
 Export.image.toDrive({
   image: comb1,
-  description: comb1,
+  description: fileNameOut,
   folder: 'gee',
   maxPixels: 1e13, 
   scale: scale,
-  region: region,
-  crs: crs,
+  region: SEI.region,
+  crs: SEI.crs,
   fileFormat: 'GeoTIFF'
 });
