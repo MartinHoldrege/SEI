5d406babbf77c871094be07e33260135b21a14ea
martinholdrege <martinholdrege@gmail.com>
2023-07-31 20:53:30 +0000

    Created scripts/02_RAP_percentiles_2drive.js.

---
+++ scripts/02_RAP_percentiles_2drive.js
@@ -0,0 +1,14 @@
+
+
+
+
+Export.image.toDrive({
+  image: comb1,
+  description: comb1,
+  folder: 'gee',
+  maxPixels: 1e13, 
+  scale: scale,
+  region: region,
+  crs: crs,
+  fileFormat: 'GeoTIFF'
+});
