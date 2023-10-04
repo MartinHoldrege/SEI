506903c2d8be0d98f67ec9408e90607e452c3ca5
mholdrege <mholdrege@gcp.usgs.gov>
2023-10-04 15:07:57 +0000

    Created 05_lyrs_for_apps.js.

---
+++ 05_lyrs_for_apps.js
@@ -0,0 +1,11 @@
+/*
+Purpose:
+Create summary data layers from data products that are used in apps, and also sampled from
+in other scripts for later analyses. 
+The main poin is that there isn't unnecessary duplication of code in app scripts
+
+Author: Martin Holdrege
+
+Started: October 4, 2023
+*/
+
