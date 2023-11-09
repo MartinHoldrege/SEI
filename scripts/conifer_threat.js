e9996da32da302d752172e3365e64ae49bbc916b
mholdrege <mholdrege@gcp.usgs.gov>
2023-11-09 16:48:41 +0000

    Created scripts/conifer_threat.js.

---
+++ scripts/conifer_threat.js
@@ -0,0 +1,14 @@
+/*
+Purpose: recreate the conifer threat layer shown in Fig. 10 in Doherty et al. 2022
+for Adam's analysis
+
+Author: Martin Holdrege
+
+Script started: Nov. 9, 2023
+
+*/
+
+// dependencies -------------------------------
+
+
+// params -------------------------------------
