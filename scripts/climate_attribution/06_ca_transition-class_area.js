647cac2b825f0dcd538c81ceb0a484a8393f3a30
mholdrege <mholdrege@gcp.usgs.gov>
2023-11-20 18:22:48 +0000

    Updated scripts/climate_attribution/06_ca_transition-class_area.js (added 56 lines, deleted 27 lines).

--- scripts/climate_attribution/06_ca_transition-class_area.js
+++ scripts/climate_attribution/06_ca_transition-class_area.js
@@ -30,41 +30,70 @@ var d = lyrMod.main({
 
 // which Q dominant driver of change ---------------------------------
 
-var q = ee.Image(d.get('qPropMean'));
-
-var driver = ee.Image(0)
-  .where(q.select('Q1raw')
-    .gt(q.select('Q2raw'))
-    .and(q.select('Q1raw').gt(q.select('Q3raw'))),
-    1) // sagebrush dominant driver of change
-  .where(q.select('Q2raw')
-    .gt(q.select('Q1raw'))
-    .and(q.select('Q2raw').gt(q.select('Q3raw'))),
-    2) // perennials
-  .where(q.select('Q3raw')
-    .gt(q.select('Q2raw'))
-    .and(q.select('Q3raw').gt(q.select('Q1raw'))),
-    3); // annuals
+// image collection of 3 banded images where bands are the proportion change 
+// of Q1-Q3, and each image is for a different GCM
+var qIc = ee.ImageCollection(d.get('qPropIc'));
+// one image per GCM, each image provides the dominant driver of change (1, 2 or 3), or 0 which is non are dominant
+var driver = qIc.map(function(x) {
+  var q = ee.Image(x);
+  var out = ee.Image(0)
+    .where(q.select('Q1raw')
+      .gt(q.select('Q2raw'))
+      .and(q.select('Q1raw').gt(q.select('Q3raw'))),
+      1) // sagebrush dominant driver of change
+    .where(q.select('Q2raw')
+      .gt(q.select('Q1raw'))
+      .and(q.select('Q2raw').gt(q.select('Q3raw'))),
+      2) // perennials
+    .where(q.select('Q3raw')
+      .gt(q.select('Q1raw'))
+      .and(q.select('Q3raw').gt(q.select('Q2raw'))),
+      3)// annuals
+    .toByte()
+    .rename('driver')
+    .copyProperties(q);
+  return out;
+});
 
 // prepare spatial index -----------------------------------------------
 
 var eco = ee.Image().paint(SEI.WAFWAecoregions, 'ecoregionNum')
   .updateMask(SEI.mask);
-  
-// first digit ecoregion, 2nd 9 class transition, 3rd which PFT most dominant driver of change
-var index = eco
-  .multiply(10)
-  .add(ee.Image(d.get('p')).select('p6_c9Med'))
-  .multiply(10)
-  .add(driver)
-  .updateMask(SEI.mask)
-  .rename('index')
-  .toInt();
-  
+
+// first digit ecoregion, 2nd 9 class transition (last digit is 0, and is 'empty')
+var ecoC9 = ee.ImageCollection(d.get('c9Ic')).map(function(x) {
+  var c9 = ee.Image(x);
+  var out = eco
+    .multiply(10)
+    .add(c9)
+    .multiply(10)
+    .rename('ecoC9')
+    .copyProperties(c9);
+  return out;
+});
+
+
+// making the 3rd digit  which PFT most dominant driver of change
+var index = ecoC9.combine(driver).map(function(x) {
+  var image = ee.Image(x);
+  var out = image.select('ecoC9')
+    .add(image.select('driver'))
+    .updateMask(SEI.mask)
+    .rename('index')
+    .toInt()
+    .copyProperties(ee.Image(x));
+  return out;
+});
+
 
 // calculate area for unique values of the spatial index ----------------
 
-var areaFc1 = fnsRr.areaByGroup(index, 'index', SEI.region, 10000);
+var areaFc1 = index.map(function(x) {
+  var image = ee.Image(x)
+  var areas1 = fnsRr.areaByGroup(image, 'index', SEI.region, 10000);
+  
+  
+}
 
 var areaFc2 = areaFc1.map(function(x) {
   var out = ee.Feature(x)
