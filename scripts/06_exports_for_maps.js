ea0db7cd1739574fe3cfff5d3b803544f7aaee74
mholdrege <mholdrege@gcp.usgs.gov>
2023-12-18 23:41:58 +0000

    output image strings corrected

--- scripts/06_exports_for_maps.js
+++ scripts/06_exports_for_maps.js
@@ -19,19 +19,29 @@ var root_co21 = 'fire1_eind1_c4grass1_co21_2311_';
 var root_grass0 = 'fire1_eind1_c4grass0_co20_2311_';
 
 var rcpList = ['RCP45', 'RCP45', 'RCP85', 'RCP85'];
-var epochList = ['2030-2060', '2030-2060', '2070-2100', '2070-2100']
+var rcpList = ['RCP45']; // for testing
+var epochList = ['2030-2060', '2030-2060', '2070-2100', '2070-2100'];
 // dependencies ---------------------------------------------
 
 var SEI = require("users/mholdrege/SEI:src/SEIModule.js");
 var lyrMod = require("users/mholdrege/SEI:scripts/05_lyrs_for_apps.js");
 var fnsRr = require("users/mholdrege/newRR_metrics:src/functions.js"); // has areaByGroup function
 
+// empty objects to add to while looping
 var areaComb = ee.FeatureCollection([]);
+var c9_fireComb = ee.Image([]);
+var c9DiffFireComb = ee.Image([]);
+var c9DiffCo2Comb = ee.Image([]);
+var c9DiffGrassComb = ee.Image([]);
+var diffPropComb = ee.Image([]);
+var qPropMeanComb = ee.Image([]);
+var numGoodC3Comb = ee.Image([]);
+
 for(var i = 0; i<rcpList.length; i++) {
-    
+  
   var rcp = rcpList[i];
   var epoch = epochList[i];
-  
+  var rcp_yr = rcp + '_' + epoch;
   
   // read in data --------------------------------------------
   // other than those specified, using the default args
@@ -39,27 +49,17 @@ for(var i = 0; i<rcpList.length; i++) {
   var d_fire0 = lyrMod.main({root: root_fire0, rcp: rcp, epoch: epoch}); 
   var d_co21 = lyrMod.main({root: root_co21, rcp: rcp, epoch: epoch}); 
   var d_grass0 = lyrMod.main({root: root_grass0, rcp: rcp, epoch: epoch});
+  var v = d_fire1.get('versionFull').getInfo() + '_';
   
   // c9 layer ------------------------------------------------
   
-  var c9_fire1 = ee.Image(d_fire1.get('p')).select('p6_c9Med');
+  var c9_fire1 = ee.Image(d_fire1.get('p'))
+    .select('p6_c9Med')
+    .rename(rcp_yr);
   
-  var s0 = d_fire1.get('versionFull').getInfo() + '_9ClassTransition_'
-    + resolutionOutC9 + '_' + d_fire1.get('root').getInfo()
-    + d_fire1.get('RCP').getInfo()  + '_' + d_fire1.get('epoch').getInfo();
-  
-  Export.image.toDrive({
-    image: c9_fire1,
-    description: s0,
-    folder: 'gee',
-    maxPixels: 1e13, 
-    scale: resolutionOutC9,
-    region: SEI.region,
-    crs: SEI.crs,
-    fileFormat: 'GeoTIFF'
-  });
-  
-  
+  var c9_fireComb = c9_fireComb
+    .addBands(c9_fire1)
+
   // c9 fire difference layer -------------------------------------
   // to show where habitat classification is different
   
@@ -70,26 +70,12 @@ for(var i = 0; i<rcpList.length; i++) {
   // where are c9 transition different? (1 = same transition & same SEI
   // 2 = same transition, but fire better SEI
   // 3 = same transition, but fire worse SEI, 4= fire1 better transition, 5 = fire1 worse transition)
-  var c9Diff = SEI.compareFutures(c9_fire1, c9_fire0, dQ5s_fire1, dQ5s_fire0);
-      
-  var s = d_fire1.get('versionFull').getInfo() + '_9ClassTransition_'
-    + resolutionOut + '_' + d_fire1.get('root').getInfo()
-    + d_fire1.get('RCP').getInfo()  + '_' + d_fire1.get('epoch').getInfo();
-    
-  var sDiff = s.replace('9ClassTransition', 'c9-diff')
-    .replace('fire1', 'fire01');
+  var c9Diff = SEI.compareFutures(c9_fire1, c9_fire0, dQ5s_fire1, dQ5s_fire0)
+    .rename(rcp_yr);
   
-  Export.image.toDrive({
-    image: c9Diff,
-    description: sDiff,
-    folder: 'gee',
-    maxPixels: 1e13, 
-    scale: resolutionOut,
-    region: SEI.region,
-    crs: SEI.crs,
-    fileFormat: 'GeoTIFF'
-  });
-    
+  var c9DiffFireComb = c9DiffFireComb
+    .addBands(c9Diff);
+  
   // c9 co2 difference layer -------------------------------------
   // to show where habitat classification is different
   
@@ -97,22 +83,12 @@ for(var i = 0; i<rcpList.length; i++) {
   var dQ5s_co21 = ee.Image(d_co21.get('diffRed2')).select('Q5s_median'); // median change in SEI
   
   // where are c9 transition different? 
-  var c9DiffCo2 = SEI.compareFutures(c9_fire1, c9_co21, dQ5s_fire1, dQ5s_co21);
-        
-  var sDiffCo2 = s.replace('9ClassTransition', 'c9-diff')
-    .replace('co20', 'co201');
-  
-  Export.image.toDrive({
-    image: c9DiffCo2,
-    description: sDiffCo2,
-    folder: 'gee',
-    maxPixels: 1e13, 
-    scale: resolutionOut,
-    region: SEI.region,
-    crs: SEI.crs,
-    fileFormat: 'GeoTIFF'
-  });
-  
+  var c9DiffCo2 = SEI.compareFutures(c9_fire1, c9_co21, dQ5s_fire1, dQ5s_co21)
+    .rename(rcp_yr);
+    
+  var c9DiffCo2Comb = c9DiffCo2Comb
+    .addBands(c9DiffCo2);
+    
   // c9 c4grass1 difference layer -------------------------------------
   // to show where habitat classification is different when c4 grass expansion is not allowed
   
@@ -120,78 +96,37 @@ for(var i = 0; i<rcpList.length; i++) {
   var dQ5s_grass0 = ee.Image(d_grass0.get('diffRed2')).select('Q5s_median'); // median change in SEI
   
   // where are c9 transition different? 
-  var c9DiffGrass = SEI.compareFutures(c9_fire1, c9_grass0, dQ5s_fire1, dQ5s_grass0);
+  var c9DiffGrass = SEI.compareFutures(c9_fire1, c9_grass0, dQ5s_fire1, dQ5s_grass0)
+    .rename(rcp_yr);
         
-  var sDiffGrass = s.replace('9ClassTransition', 'c9-diff')
-    .replace('grass1', 'grass01');
-  
-  Export.image.toDrive({
-    image: c9DiffGrass,
-    description: sDiffGrass,
-    folder: 'gee',
-    maxPixels: 1e13, 
-    scale: resolutionOut,
-    region: SEI.region,
-    crs: SEI.crs,
-    fileFormat: 'GeoTIFF'
-  });
-  
+  var c9DiffGrassComb = c9DiffGrassComb
+    .addBands(c9DiffGrass);
   // proportion change layers -------------------------------------
   // (proportion change from current to future conditions)
   
   var diffProp = ee.Image(d_fire1.get('diffPropRed'))
-    .select('Q\\draw_median', 'Q5s_median');
-    
-  var sDiff = s.replace('9ClassTransition', 'diffProp');
+    .select('Q\\draw_median', 'Q5s_median')
+    .regexpRename('$', '_' + rcp_yr)
   
-  Export.image.toDrive({
-    image: diffProp,
-    description: sDiff,
-    folder: 'gee',
-    maxPixels: 1e13, 
-    scale: resolutionOut,
-    region: SEI.region,
-    crs: SEI.crs,
-    fileFormat: 'GeoTIFF'
-  });
+  var diffPropComb = diffPropComb
+    .addBands(diffProp);
   
   // qProp layer (for RGB maps) -----------------------------------
   
-  var qPropMean = ee.Image(d_fire1.get('qPropMean'));
-  
-  var sQProp = s.replace('9ClassTransition', 'qPropMean');
-  
-  Export.image.toDrive({
-    image: qPropMean,
-    description: sQProp,
-    folder: 'gee',
-    maxPixels: 1e13, 
-    scale: resolutionOut,
-    region: SEI.region,
-    crs: SEI.crs,
-    fileFormat: 'GeoTIFF'
-  });
+  var qPropMeanComb = ee.Image(d_fire1.get('qPropMean'))
+    .regexpRename('$', '_' + rcp_yr)
+    .addBands(qPropMeanComb);
   
   // climate confidence layers -----------------------------------
   // for areas that are currently core the the number of GCMs that agree that will be core in the futre
   // for areas that are currently grow, the number of GCMS that agree that will be Core or Grow in the future
   
   // first digit is c3 classification, 2nd and 3rd digit is number of gcms that suggest things get better or stay the same (for grows and cores)
-  var numGoodC3 = ee.Image(d_fire1.get('numGcmGood'));
+  var numGoodC3Comb = ee.Image(d_fire1.get('numGcmGood'))
+    .regexpRename('$', '_' + rcp_yr)
+    .addBands(numGoodC3Comb);
     
-  var sNumGood = s.replace('9ClassTransition', 'numGcmGood'); 
-    
-  Export.image.toDrive({
-    image: numGoodC3,
-    description: sNumGood,
-    folder: 'gee',
-    maxPixels: 1e13, 
-    scale: resolutionOut,
-    region: SEI.region,
-    crs: SEI.crs,
-    fileFormat: 'GeoTIFF'
-  });
-  
+
   // calculating c9-diff area -----------------------------------
   // calculating the area in the 5 categories of the transition comparison lyrs;
   // in all cases the comparison is to the 'default' simulations
@@ -225,7 +160,95 @@ for(var i = 0; i<rcpList.length; i++) {
 
 } // end of RCP, epoch loop
 
+// images with bands for each rcp/epoch ------------------------
+// c9 
+Export.image.toDrive({
+  image: c9_fireComb,
+  description: v + '9ClassTransitionMed_' + resolutionOutC9 + '_' + root_fire1.replace(/_$/, ""),
+  folder: 'gee',
+  maxPixels: 1e13, 
+  scale: resolutionOutC9,
+  region: SEI.region,
+  crs: SEI.crs,
+  fileFormat: 'GeoTIFF'
+});
+  
+var outString = function(description, root) {
+  var rootShort = root.replace(/_$/, "");
+  return v + description + '_' + resolutionOut + '_' +  rootShort;
+};
+// c9 diff fire
+Export.image.toDrive({
+  image: c9DiffFireComb,
+  description: outString('c9-diff', root_fire1.replace('fire1', 'fire01')),
+  folder: 'gee',
+  maxPixels: 1e13, 
+  scale: resolutionOut,
+  region: SEI.region,
+  crs: SEI.crs,
+  fileFormat: 'GeoTIFF'
+});
 
+// c9 diff co2
+Export.image.toDrive({
+  image: c9DiffCo2Comb,
+  description: outString('c9-diff', root_co21.replace('co21', 'co201')),
+  folder: 'gee',
+  maxPixels: 1e13, 
+  scale: resolutionOut,
+  region: SEI.region,
+  crs: SEI.crs,
+  fileFormat: 'GeoTIFF'
+});
+
+// c9 diff grass
+Export.image.toDrive({
+  image: c9DiffGrassComb,
+  description: outString('c9-diff', root_grass0.replace('grass0', 'grass01')),
+  folder: 'gee',
+  maxPixels: 1e13, 
+  scale: resolutionOut,
+  region: SEI.region,
+  crs: SEI.crs,
+  fileFormat: 'GeoTIFF'
+});
+
+// proportional changes in Q & SEI
+Export.image.toDrive({
+  image: diffPropComb,
+  description: outString('diffProp', root_fire1),
+  folder: 'gee',
+  maxPixels: 1e13, 
+  scale: resolutionOut,
+  region: SEI.region,
+  crs: SEI.crs,
+  fileFormat: 'GeoTIFF'
+});
+
+// q prop (for RGB maps)
+Export.image.toDrive({
+  image: qPropMeanComb,
+  description: outString('qPropMean', root_fire1),
+  folder: 'gee',
+  maxPixels: 1e13, 
+  scale: resolutionOut,
+  region: SEI.region,
+  crs: SEI.crs,
+  fileFormat: 'GeoTIFF'
+});
+
+Export.image.toDrive({
+  image: numGoodC3Comb,
+  description: outString('numGcmGood', root_fire1),
+  folder: 'gee',
+  maxPixels: 1e13, 
+  scale: resolutionOut,
+  region: SEI.region,
+  crs: SEI.crs,
+  fileFormat: 'GeoTIFF'
+});
+
+// area -------------------------------------------------
 // outputting Fc with results from all iterations of the loop
 var s = d_fire1.get('versionFull').getInfo() + '_20231206';
 
