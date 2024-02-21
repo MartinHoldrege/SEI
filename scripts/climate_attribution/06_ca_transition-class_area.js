342a46ccab87050317be95aa1415a6f0dfca80be
mholdrege <mholdrege@gcp.usgs.gov>
2024-02-21 17:57:05 +0000

    Updated scripts/climate_attribution/06_ca_transition-class_area.js (added 4 lines, deleted 3 lines).

--- scripts/climate_attribution/06_ca_transition-class_area.js
+++ scripts/climate_attribution/06_ca_transition-class_area.js
@@ -53,7 +53,7 @@ if (testRun) {
 // then that pixel is not masked, otherwise it is, 
 // this is then applied to an IC, and median taken, so that you get the
 // driver that corresponds to e.g. the GCM with the low, median, or high SEI for the given pixel
-var maskDriverFactory = function(redImage, reducerName) {
+var maskSeiRedFactory = function(redImage, reducerName, bandNames) {
   var f = function(x) {
     var image = ee.Image(x);
     var mask = image.select('Q5s')
@@ -63,7 +63,7 @@ var maskDriverFactory = function(redImage, reducerName) {
       // then assume that is the correct GCM
       .lt(0.001) 
       .rename(reducerName);
-    return image.select('driver').updateMask(mask);
+    return image.select(bandNames).updateMask(mask);
   };
   return f;
 };
@@ -157,7 +157,8 @@ for (var i = 0; i < roots.length; i++) {
       .select('Q5s')
       .filter(ee.Filter.eq('GCM', reducerName))
       .mean()
-    var f = maskDriverFactory(image, reducerName);
+    // creates a function only keeps the value from the GCM that corresponds to the median (or low or high) SEI
+    var f = SEI.maskSeiRedFactory(image, reducerName, 'driver'); 
     return driver1.map(f)
       .reduce(ee.Reducer.median())
       .rename('driver')
