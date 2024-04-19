3eb42f202e7da1d57fb41f3c950eb5f17bfdb198
martinholdrege <martinholdrege@gmail.com>
2024-04-19 15:24:01 +0000

    updaced to c9-diff lyrs export again

--- scripts/06_exports_for_maps.js
+++ scripts/06_exports_for_maps.js
@@ -61,14 +61,21 @@ for(var i = 0; i<rcpList.length; i++) {
     .rename(rcp_yr);
   
   var c9_fireComb = c9_fireComb
-    .addBands(c9_fire1)
-
+    .addBands(c9_fire1);
+  
   // c9 fire difference layer -------------------------------------
   // to show where habitat classification is different
   
   var c9_fire0 = ee.Image(d_fire0.get('p')).select('p6_c9Med');
-  var dQ5s_fire0  = ee.Image(d_fire0.get('diffRed2')).select('Q5s_median'); // median change in SEI
-  var dQ5s_fire1  = ee.Image(d_fire1.get('diffRed2')).select('Q5s_median'); // median change in SEI
+   
+  var tst = ee.ImageCollection(d_fire0.get('diffRed'))
+  //print(tst)
+  // print(SEI.ic2Image(tst, 'GCM'))
+  var dQ5s_fire0  = SEI.ic2Image(ee.ImageCollection(d_fire0.get('diffRed')), 'GCM')
+    .select('Q5s_median'); // median change in SEI
+   
+  var dQ5s_fire1  = SEI.ic2Image(ee.ImageCollection(d_fire1.get('diffRed')), 'GCM')
+    .select('Q5s_median'); // median change in SEI
   
   // where are c9 transition different? (1 = same transition & same SEI
   // 2 = same transition, but fire better SEI
@@ -83,7 +90,9 @@ for(var i = 0; i<rcpList.length; i++) {
   // to show where habitat classification is different
   
   var c9_co21 = ee.Image(d_co21.get('p')).select('p6_c9Med');
-  var dQ5s_co21 = ee.Image(d_co21.get('diffRed2')).select('Q5s_median'); // median change in SEI
+  
+  var dQ5s_co21 = SEI.ic2Image(ee.ImageCollection(d_co21.get('diffRed')), 'GCM')
+    .select('Q5s_median');
   
   // where are c9 transition different? 
   var c9DiffCo2 = SEI.compareFutures(c9_fire1, c9_co21, dQ5s_fire1, dQ5s_co21)
@@ -96,7 +105,8 @@ for(var i = 0; i<rcpList.length; i++) {
   // to show where habitat classification is different when c4 grass expansion is not allowed
   
   var c9_grass0 = ee.Image(d_grass0.get('p')).select('p6_c9Med');
-  var dQ5s_grass0 = ee.Image(d_grass0.get('diffRed2')).select('Q5s_median'); // median change in SEI
+  var dQ5s_grass0 = SEI.ic2Image(ee.ImageCollection(d_grass0.get('diffRed')), 'GCM')
+    .select('Q5s_median');
   
   // where are c9 transition different? 
   var c9DiffGrass = SEI.compareFutures(c9_fire1, c9_grass0, dQ5s_fire1, dQ5s_grass0)
@@ -278,7 +288,7 @@ Export.image.toDrive({
 
 // area -------------------------------------------------
 // outputting Fc with results from all iterations of the loop
-var s = d_fire1.get('versionFull').getInfo() + '_20231220';
+var s = d_fire1.get('versionFull').getInfo() + '_20240419';
 
 Export.table.toDrive({
   collection: areaComb,
