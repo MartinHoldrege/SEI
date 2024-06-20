9bc28e62f32a823420c0a6b0b5078d9b1b602e2a
Martin Holdrege <martinholdrege@gmail.com>
2024-06-20 09:01:24 -0600

    created matching quality lyr for data release (and pub qual fig for appendix)

--- scripts/matching-quality.R
+++ scripts/matching-quality.R
@@ -25,27 +25,32 @@ template <- rast("../grazing_effects/data_processed/interpolation_data/cellnumbe
 qual1 <- read_csv('../grazing_effects/data_processed/interpolation_data/match-qual_v1-interp_v3-criteria.csv')
 
 # c9 layer (to convert to c3)
-c9 <- rast('data_processed/transitions/vsw4-3-4_9ClassTransitionMed_180_fire1_eind1_c4grass1_co20_2311.tif')
+# use one of the c9 layers prepared for the data release
+path_release <- 'C:/Users/mholdrege/OneDrive - DOI/scd_data_publication2'
+c9 <- rast(file.path(path_release, "c9_Default_RCP45_2071-2100.tif"))
+c9 <- c9[['c9_median']]
 
-# temp (for testing)
-# c9 <- resample(c9, template)
+# c9 <- aggregate(c9, fact = 10, fun = 'modal') # (for testing)
+
 # prepare layers ----------------------------------------------------------
 
 # *c3 ---------------------------------------------------------------------
-c9 <- c9[['RCP45_2070-2100']]
+
 c3 <- subst(c9, 
             from = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 0),
-            to   = c(1, 1, 1, 2, 2, 2, 3, 3, 3, NA))
+            to   = c(1L, 1L, 1L, 2L, 2L, 2L, 3L, 3L, 3L, NA_integer_))
 # * matching quality ------------------------------------------------------
-# this is the 'mask' at 180 m resolution
 
 r_qual1 <- fill_raster(df = qual1, template = template)
 r_qual2 <- resample(r_qual1, c3)
-r_qual3 <- terra::mask(r_qual2, c3) # scd mask
+r_qual_full <- terra::mask(r_qual2, c3) # scd mask, full resolution
+
+r_qual3 <- terra::aggregate(r_qual_full, fact = 3) # lower resolution for plotting (lower resolution, and additional )
+c3_agg <- terra::aggregate(c3, fact =3, fun = 'modal') 
 
 # masking to just show core or grow
-r_qual_core <- terra::mask(r_qual2, c3, maskvalues = c(NA, 2, 3))
-r_qual_grow <- terra::mask(r_qual2, c3, maskvalues = c(NA, 1, 3))
+r_qual_core <- terra::mask(r_qual3, c3_agg, maskvalues = c(NA, 2, 3))
+r_qual_grow <- terra::mask(r_qual3, c3_agg, maskvalues = c(NA, 1, 3))
 plot(r_qual_grow)
 
 # figures of matching quality ---------------------------------------------
@@ -79,11 +84,26 @@ g3 <- plot_map2(r_qual3,
   match_fill()
 
 jpeg('figures/matching_quality/match-qual_Renne-criteria.jpg',
-     res = 800, width = 8, height = 8, units = 'in')
+     res = 800, width = 7, height = 7, units = 'in')
 design <- "
 AB
-C#
+CD
 "
-g1 + g2 + g3 + patchwork::plot_layout(design = design) +
-  plot_annotation(caption = 'Matching quality calculated using Criteria from Renne et al. 2024')
+g1 + g2 + g3 + guide_area()  + 
+  patchwork::plot_layout(design = design, guides = 'collect') 
 dev.off()
+
+# file for data release ---------------------------------------------------
+
+writeRaster(r_qual_full, file.path(path_release, 'matching_quality.tif'),
+            filetype = "COG", wopt = list(gdal = "COMPRESS=DEFLATE"),
+            datatype = 'FLT4S',
+            overwrite = TRUE)
+
+
+if(FALSE) {
+  r <- rast(file.path(path_release, 'matching_quality.tif'))
+  r
+  plot(r)
+  plot(r > 1.5) # where matching quality is poor
+}
