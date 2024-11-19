84f44aebb8fa67001a468640387d0516e8310439
martinholdrege <martinholdrege@gmail.com>
2024-11-19 20:22:22 +0000

    Updated scripts/GYE/01_GYE_SEI.js (added 16 lines, deleted 3 lines).

--- scripts/GYE/01_GYE_SEI.js
+++ scripts/GYE/01_GYE_SEI.js
@@ -12,13 +12,26 @@
 // parameters ----------------------------------------------------------
 
 var path = 'projects/usgs-gee-drylandecohydrology/assets/SEI/'
+
+// dependencies -------------------------------------------------------
+
+var SEI = require("users/mholdrege/SEI:src/SEIModule.js");
+
 // load data -----------------------------------------------------------
 
 var forMask = ee.Image(path + 'GYE/Ecostate_SagebrushBiome_v4_21_23')
-print(forMask)
+
 
 var region = ee.FeatureCollection(path + 'GYE/GRYN_GYA_Boundary_AOA_Area')
 print(region)
 
-Map.addLayer(region, {}, 'region')
-Map.addLayer(forMask, {min: 0, max: 7}, 'ecostate')
+Map.addLayer(region, {}, 'region', false)
+Map.addLayer(forMask, {min: 0, max: 7}, 'ecostate', false)
+
+var cur = ee.Image('projects/fws-gee-sagebrush/assets/2023_SCD_v30_20230828/SEI_v30_2018_2021_90_20230828')
+
+print(cur.bandNames())
+
+Map.addLayer(cur.select('sage560m').selfMask(), {min: 0, max: 30}, 'sage')
+Map.addLayer(Q, {min: 0, max: 30}, 'sage')
+Map.addLayer(SEI.mask, {palette: 'blue'}, 'mask')
