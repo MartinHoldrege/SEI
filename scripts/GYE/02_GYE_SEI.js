d159e26fa9ce906ed3d0b051c9f8430b76bafb65
mholdrege <mholdrege@gcp.usgs.gov>
2024-11-25 18:01:01 +0000

    Updated scripts/GYE/02_GYE_SEI.js (added 21 lines, deleted 11 lines).

--- scripts/GYE/02_GYE_SEI.js
+++ scripts/GYE/02_GYE_SEI.js
@@ -1,24 +1,34 @@
 /*Purpose: Recreate the main SCD layers for the greater yellowstone ecostem (GYE)
   This is using v3.0 of the SCD, but with a different (less expansive mask)
   
-  Author: Martin Holdrege
+  Author: Martin Holdrege (adapting code written by Dave Theobald)
   
   Date started: Nov. 11, 2024
 
-
 */
 
+// dependencies -------------------------------------------------------
+
+var SEI = require("users/mholdrege/SEI:src/SEIModule.js");
 
 // parameters ----------------------------------------------------------
 
-var path = 'projects/usgs-gee-drylandecohydrology/assets/SEI/'
+var path = 'projects/usgs-gee-drylandecohydrology/assets/SEI/';
+
 // load data -----------------------------------------------------------
 
-var forMask = ee.Image(path + 'GYE/Ecostate_SagebrushBiome_v4_21_23')
-print(forMask)
+// 2019-2021 threat based ecostates file (provided by B. Sparklin)
+var forMask = ee.Image(path + 'GYE/ThreatBasedEcostates_GYE');
+var region = ee.FeatureCollection(path + 'GYE/GRYN_GYA_Boundary_AOA_Area');
 
-var region = ee.FeatureCollection(path + 'GYE/GRYN_GYA_Boundary_AOA_Area')
-print(region)
 
-Map.addLayer(region, {}, 'region')
-Map.addLayer(forMask, {min: 0, max: 7}, 'ecostate')
+
+// prepare mask -------------------------------------------------------
+
+var mask = forMask.gt(0).selfMask();
+Map.addLayer(mask, {palette: 'blue'}, 'mask', false)
+
+SEI.tundra
+
+
+
