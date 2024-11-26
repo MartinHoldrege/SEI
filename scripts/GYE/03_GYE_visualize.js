1d272685727fc52b7f07aa5f34acfcce17806052
mholdrege <mholdrege@gcp.usgs.gov>
2024-11-26 00:12:46 +0000

    Created scripts/GYE/03_GYE_visualize.js.

---
+++ scripts/GYE/03_GYE_visualize.js
@@ -0,0 +1,15 @@
+/*
+  Purpose: Examine the GYE SEI layers, and compare
+  to the regular (v30 SEI).
+  Note that the v30 SEI layers were made with a different
+  mask, and 4 year evenly weighted averages of rap 
+  cover were taken instead of unevenly weighted (like
+  the regular v30 assets)
+
+*/
+
+// read in data ------------------------------------------------------
+
+// 
+
+
