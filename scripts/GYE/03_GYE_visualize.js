a989b9a8cff6998dd87b3768a477bb928778cbd0
mholdrege <mholdrege@gcp.usgs.gov>
2024-11-26 17:53:26 +0000

    Updated scripts/GYE/03_GYE_visualize.js (added 27 lines, deleted 7 lines).

--- scripts/GYE/03_GYE_visualize.js
+++ scripts/GYE/03_GYE_visualize.js
@@ -8,17 +8,37 @@
 
 */
 
+// dependencies -------------------------------------------------------
+
+var fig = require("users/mholdrege/SEI:src/fig_params.js");
+
+// params -------------------------------------------------------------
+
+var path = 'projects/usgs-gee-drylandecohydrology/assets/SEI/';
 // read in data ------------------------------------------------------
 
 // for comparison purposes
 var v30 = ee.Image('projects/fws-gee-sagebrush/assets/2023_SCD_v30_20230828/SEI_v30_2018_2021_90_20230828')
-var gye = ee.Image('projects/usgs-gee-drylandecohydrology/assets/SEI/GYE/v30/SEI_v30_2018_2021_30_GYE_ecoStateMask_20241126')
+var gye = ee.Image(path + 'GYE/v30/SEI_v30_2018_2021_30_GYE_ecoStateMask_20241126')
+var region = ee.FeatureCollection(path + 'GYE/GRYN_GYA_Boundary_AOA_Area');
 
-print(v30.bandNames());
-print(gye.bandNames())
+// print(gye.bandNames())
 
+// functions -----------------------------------------------------
+
+var addMap = function(band, vis) {
+  Map.addLayer(gye.select(band), fig.visc3, band + ' gye', false)
+  Map.addLayer(v30.select(band), fig.visc3, band + ' v30', false)
+}
+
+// prepare data --------------------------------------------------
+
+var gye = gye.clip(region)
+var v30 = v30.clip(region) // for eesier comparison
 // maps ---------------------------------------------------------
 
+Map.centerObject(gye)
 
-
+// c3
+addMap('Q5sc3', fig.visc3)
 
