ea95b6f01da6d64694ac9bb35575090136eb5792
martinholdrege <martinholdrege@gmail.com>
2025-05-05 22:26:30 +0000

    Created scripts/write_low_res_for_graze_project.js.

---
+++ scripts/write_low_res_for_graze_project.js
@@ -0,0 +1,17 @@
+var resolution = 1000
+
+var SEI = require("users/MartinHoldrege/SEI:src/SEIModule.js");
+var scdImage = ee.Image('projects/fws-gee-sagebrush/assets/2022_SCD_OFR_20211217/SEIv11_2017_2020_30_20211228')
+
+var scdImage = scdImage.select(['.*raw', '.*560m', 'Q5s', 'Q5sc3'])
+
+Export.image.toDrive({
+  image: scdImage.toFloat(),
+  description: 'SEIv11_2017_2020_' + resolution + '_20211228',
+  folder: 'SEI',
+  maxPixels: 1e13, 
+  scale: resolution,
+  region: SEI.region,
+  crs: SEI.crs,
+  fileFormat: 'GeoTIFF'
+});
