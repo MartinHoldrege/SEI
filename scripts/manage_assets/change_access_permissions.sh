a5b8fb37925a06e6b3de575d2f73e27c569d7026
Martin Holdrege <martinholdrege@gmail.com>
2026-04-28 15:56:17 -0600

    minor settings

--- scripts/manage_assets/change_access_permissions.sh
+++ scripts/manage_assets/change_access_permissions.sh
@@ -13,7 +13,7 @@ source $".bashrc" # b/ of aliases etc. for conda environment (just needed b/ idi
 conda activate ee # earthengine commandline environment
 
 # Set the Earth Engine asset folder
-FOLDER="projects/usgs-gee-drylandecohydrology/assets/SEI/stepwat_biomass" #earth engine directory
+FOLDER="projects/usgs-gee-drylandecohydrology/assets/SEI/vsw4-3/products/" #earth engine directory
 
 earthengine authenticate
 earthengine set_project "usgs-gee-drylandecohydrology"
@@ -25,11 +25,10 @@ while read -r ASSET; do
   echo "$ASSET"
     # I'm not quite sure why eval echo was needed here but didn't work without
   #$(eval echo earthengine acl set public $ASSET) # provide public read access (not working)
-  COMMAND="earthengine acl set private $ASSET"
-
+  COMMAND="earthengine acl set public $ASSET"
   eval $COMMAND # not sure why the straight 'eval' didn't work but this is working now
-  COMMAND="earthengine acl ch -u martinholdrege@gmail.com:W $ASSET" # granting write permissions
-  eval $COMMAND 
+  #COMMAND="earthengine acl ch -u martinholdrege@gmail.com:W $ASSET" # granting write permissions
+  #eval $COMMAND 
 done < temp_assets.txt
 
 
